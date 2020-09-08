2387f9977edeae7d6239b3b847a731a10e9e440e
Matthijs <matthijskroon@gmail.com>
2020-09-09 00:05:00 +0200

    SplitText example

--- example/src/App.js
+++ example/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { Gutter, MaxWidth, Spacer } from '@mdkroon/react-ui-components'
+import { Gutter, MaxWidth, Spacer, SplitText } from '@mdkroon/react-ui-components'
 import style from './app.module.css'
 
 const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
+const plainText = 'This text contains new line characters.\nThis begins on a new line.\rThis begins on a new line.\r\nThis begins on a new line.'
 
 const App = () => {
   return (
@@ -29,6 +31,13 @@ const App = () => {
           </div>
         </Gutter>
       </MaxWidth>
+      <Spacer height={12}/>
+      SplitText with span:<br />
+      <SplitText type='em'>{plainText}</SplitText>
+      <Spacer height={12}/>
+      SplitText with p:<br />
+      <SplitText breaks={false} type='p'>{plainText}</SplitText>
+      <Spacer height={12}/>
     </div>
   )
 }
