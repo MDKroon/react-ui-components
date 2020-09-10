0eca9c7e82c0ab1e49dad205a37fb447a31d01bc
Matthijs <matthijskroon@gmail.com>
2020-09-10 21:08:45 +0200

    move to text folder

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ export { default as Gutter } from './components/layout/Gutter'
 export { default as MaxWidth } from './components/layout/MaxWidth'
 export { default as Spacer } from './components/layout/Spacer'
 
-// misc
-export { default as SplitText } from './components/misc/SplitText'
+// text
+export { default as SplitText } from './components/text/SplitText'
